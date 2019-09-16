fed65416e9b01661b02916d5a09bb58b3e9aec87
LytvynovAnton <manunited150891@gmail.com>
2019-09-16 14:24:20 +0300

    Add import Root to index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import Root from './containers/Root';
 
 let knex = require('knex')({
   client: 'sqlite3', connection: { filename: './tasks.sqlite' }
